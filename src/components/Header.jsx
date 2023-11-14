15a02a42de9d8fa9a66133f4d0de01436fe846b7
nidhi2793 <MY_NAME@example.com>
2023-11-15 03:32:31 +0530

    Point logo to \

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 export const Header = (showSearch) => {
   return (
     <div className="header">
-      <img
-        src="/images/starwarslogo.png"
-        alt="starwars logo"
-        className="starwarslogo"
-      />
+      <Link to="/">
+        <img
+          src="/images/starwarslogo.png"
+          alt="starwars logo"
+          className="starwarslogo"
+        />
+      </Link>
       {showSearch && <div>Search</div>}
     </div>
   );
